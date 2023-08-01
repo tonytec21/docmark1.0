e6e44d216792d0180221df188214f5aa75b20df5
Tony Tec <88214774+tonytec21@users.noreply.github.com>
2023-08-01 01:58:02 -0300

    oo

--- pdf-para-tiff/historico.php
+++ pdf-para-tiff/historico.php
@@ -46,12 +46,12 @@ for ($i = $minimo; $i <= $maximo; $i++) {
     <script src="js/chart.js"></script>
     <script src="js/chartjs-plugin-datalabels.js"></script>
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="menu.js"></script>
 
     <script type="text/javascript" charset="utf8" src="js/jquery-3.5.1.js"></script>
     <script type="text/javascript" charset="utf8" src="js/jquery.dataTables.js"></script>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="menu.js"></script>
 
 
 

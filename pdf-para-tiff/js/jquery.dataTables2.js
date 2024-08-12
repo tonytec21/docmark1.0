c46ff00bd5ed044a0fbb7dca94bfdfd69b971823
Tony Tec <tonytec@hotmail.com.br>
2024-08-12 08:10:47 -0300

    x

--- pdf-para-tiff/js/jquery.dataTables2.js
+++ pdf-para-tiff/js/jquery.dataTables2.js
@@ -11645,7 +11645,7 @@
 			 *      } );
 			 *    } );
 			 */
-			"sInfo": "Mostrando _START_ a _END_ de _TOTAL_ erros",
+			"sInfo": "Mostrando _START_ a _END_ de _TOTAL_ indicadores",
 	
 	
 			/**
@@ -11666,7 +11666,7 @@
 			 *      } );
 			 *    } );
 			 */
-			"sInfoEmpty": "Mostrando 0 a 0 de 0 erros",
+			"sInfoEmpty": "Mostrando 0 a 0 de 0 indicadores",
 	
 	
 			/**
@@ -11688,7 +11688,7 @@
 			 *      } );
 			 *    } );
 			 */
-			"sInfoFiltered": "(filtrado de _MAX_ erros totais)",
+			"sInfoFiltered": "(filtrado de _MAX_ indicadores totais)",
 	
 	
 			/**
@@ -11806,7 +11806,7 @@
 			 *      } );
 			 *    } );
 			 */
-			"sLengthMenu": "Mostrar _MENU_ erros",
+			"sLengthMenu": "Mostrar _MENU_ indicadores",
 	
 	
 			/**
